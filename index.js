738bd638d23620284bfa0cb2ee3e94859d807be9
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-10-03 13:35:03 -0600

    commit tree e5bdc57ee945becf700b4804059b0b7c003a7e32

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const gifters = ["Tiff","Tina","Edward"]
+const events = "surprise"
+
+function writeCards(gifters, events){
+    let cardArray = []
+    for(let i = 0; i < gifters.length; i++){
+    cardArray.push(`Thank you, ${gifters[i]}, for the wonderful ${events} gift!`);
+    }
+    return (cardArray)
+}
+
+writeCards(gifters,events)
+
+function countDown(startNumber){
+    while (startNumber >= 0) {
+      console.log(startNumber--);
+    }
+  }
+  countDown(10)
